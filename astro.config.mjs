d372b250bf1886984bef84da7ded9920c8a45907
Tokiya Takemoto <takemoto@third-inc.co.jp>
2023-02-16 18:16:12 +0900

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  output: "server",
+  adapter: cloudflare()
+});
